9fa76b2608fa0aab15189fa2dcdc59e5325f6cbb
nomi404 <maliknouman234@gmail.com>
2024-05-21 15:59:01 +0200

    Add project files.

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.IO;
+
+namespace ToDoList.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly string csvFilePath = "todoItems.csv";
+        private readonly string csvFilePathCompletedTasks = "doneItems.csv";
+
+        private List<TodoItem> ReadTodoItemsFromCsv(string path)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>();
+            if (System.IO.File.Exists(path))
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HeaderValidated = null,
+                };
+                using (var reader = new StreamReader(path))
+                using (var csv = new CsvReader(reader, config))
+                {
+                    todoItems = csv.GetRecords<TodoItem>().ToList();
+                }
+            }
+            return todoItems;
+        }
+
+        private void WriteTodoItemsToCsv(List<TodoItem> todoItems,string path)
+        {
+            using (var writer = new StreamWriter(path))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(todoItems);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            var todoItems = ReadTodoItemsFromCsv(csvFilePath);
+            return View(todoItems);
+        }
+        public IActionResult CompleteTask()
+        {
+            var completedTasks = ReadTodoItemsFromCsv(csvFilePathCompletedTasks);// Get the list of completed tasks from wherever they are stored
+            return View(completedTasks);
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title, string description, int days, int hours ,int minutes,Priority priority)
+        {
+            var estimatedTime = new TimeSpan(days, hours, minutes);
+            int nextId = 1;
+            var todoItems = ReadTodoItemsFromCsv(csvFilePath);
+            if (todoItems.Any())
+            {
+                nextId = todoItems.Max(t => t.id) + 1;
+            }
+            var newTodo = new TodoItem(nextId, title, description, estimatedTime, priority);
+            todoItems.Add(newTodo);
+            WriteTodoItemsToCsv(todoItems, csvFilePath);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult StartTask(int id)
+        {
+            var todoItems = ReadTodoItemsFromCsv(csvFilePath);
+            var todoItem = todoItems.FirstOrDefault(t => t.id == id);
+            if (todoItem != null)
+            {
+                todoItem.StartTask();
+                WriteTodoItemsToCsv(todoItems, csvFilePath);
+                TempData["isStarted_" + id] = todoItem.isStarted;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult StopTask(int id)
+        {
+
+            var todoItems = ReadTodoItemsFromCsv(csvFilePath);
+            var todoItem = todoItems.FirstOrDefault(t => t.id == id);
+            bool initialIsStarted = ViewData["isStarted_"] as bool? ?? false;
+
+            if (todoItem != null)
+            {
+                todoItem.StopTask();
+                WriteTodoItemsToCsv(todoItems, csvFilePath);
+                TempData["isStarted_" + id] = todoItem.isStarted;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult MarkComplete(int id)
+        {
+            var todoItems = ReadTodoItemsFromCsv(csvFilePath);
+            var doneItems = ReadTodoItemsFromCsv(csvFilePathCompletedTasks);
+            var todoItem = todoItems.FirstOrDefault(t => t.id == id);
+            if (todoItem != null)
+            {
+                todoItem.CloseTask();
+                doneItems.Add(todoItem);
+                WriteTodoItemsToCsv(doneItems, csvFilePathCompletedTasks);
+                Delete(id);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id, string description)
+        {
+            var todoItems = ReadTodoItemsFromCsv(csvFilePath);
+            var todoItem = todoItems.FirstOrDefault(t => t.id == id);
+            if (todoItem != null)
+            {
+                todoItem.description = description;
+                WriteTodoItemsToCsv(todoItems, csvFilePath);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id) 
+        {
+            var todoItems = ReadTodoItemsFromCsv(csvFilePath);
+            var todoItem = todoItems.FirstOrDefault(t => t.id == id);
+            if (todoItem !=null) 
+            {
+                todoItems.Remove(todoItem);
+                WriteTodoItemsToCsv(todoItems, csvFilePath);
+               
+            }
+            return RedirectToAction("Index");   
+        }
+
+        public IActionResult UpdatePriority(int id,Priority priority)
+        {
+            var todoItems = ReadTodoItemsFromCsv(csvFilePath);
+            var todoItem = todoItems.FirstOrDefault(t=>t.id == id);
+            if (todoItem != null) 
+            {
+                todoItem.priority = priority;   
+                WriteTodoItemsToCsv(todoItems,csvFilePath);
+            }
+            return RedirectToAction("Index");   
+        }
+    }
+}
---
+++ Models/ToDoItem.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using CsvHelper.Configuration.Attributes;
+namespace ToDoList.Models
+{
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+    public class TodoItem
+    {
+
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateTime? logTime { get; set; }
+        public TimeSpan estimateTime { get; set; }
+        public bool isClosed { get; set; }
+        public DateTime? closeTime { get; set; }
+        [Ignore]
+        public bool isStarted { get; set; }
+        public DateTime? startTime { get; set; } // Nullable DateTime
+        public TimeSpan timeSpent { get; set; }
+
+        public Priority priority { get; set; }  
+        public TodoItem(int Id, string title, string description, TimeSpan estimateTime,Priority priority)
+        {
+            this.id = Id;
+            this.title = title;
+            this.description = description;
+            this.logTime = DateTime.Now; // Set the current time when the task is created
+            this.estimateTime = estimateTime;
+            this.closeTime = DateTime.MinValue;
+            this.startTime = DateTime.MinValue;
+            this.isClosed = false; // Newly created tasks are not closed by default
+            this.isStarted = false; // Task is not started by default
+            this.timeSpent = TimeSpan.Zero; // Initialize time spent to zero
+            this.priority = priority;   
+        }
+        public TodoItem(int id, string title, string description,DateTime logTime, TimeSpan estimateTime,bool isClosed,DateTime closeTime, DateTime startTime,TimeSpan timeSpent,Priority priority)
+        {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.logTime = logTime;
+            this.estimateTime = estimateTime;
+            this.isClosed = isClosed;
+            this.closeTime = closeTime;
+            this.startTime = startTime;
+            this.timeSpent = timeSpent;
+            this.priority = priority;
+            
+        }
+
+        public string DisplayTimeSpent
+        {
+            get
+            {
+                return $"{(int)timeSpent.TotalHours:00h}:{timeSpent.Minutes:00m}:{timeSpent.Seconds:00s}";
+            }
+        }
+        public string DisplayETimeSpent
+        {
+            get
+            {
+                return $"{(int)estimateTime.TotalHours:00h}:{estimateTime.Minutes:00m}:{estimateTime.Seconds:00s}";
+            }
+        }
+
+
+        // Method to close the task
+        public void CloseTask()
+        {
+            isClosed = true;
+            closeTime = DateTime.Now; // Set the current time when the task is closed
+        }
+
+        // Method to start the task
+        public void StartTask()
+        {
+            isStarted = true;
+            startTime = DateTime.Now; // Set the current time when the task is started
+        }
+
+        // Method to stop the task
+        public void StopTask()
+        {
+            isStarted = false;
+            if (startTime.HasValue && startTime.Value != DateTime.MinValue)
+            {
+                // Calculate time spent on the task and add the time already spent
+                timeSpent += (DateTime.Now - startTime.Value);
+            }
+        }
+    }
+}
